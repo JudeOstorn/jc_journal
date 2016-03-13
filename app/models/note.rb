196d3e001f9731de14c77a9e58326e044d044354
Jude <judeostorn66@gmail.com>
2016-03-14 03:48:17 +0500

    Update front

--- app/models/note.rb
+++ app/models/note.rb
@@ -9,7 +9,7 @@ class Note < ActiveRecord::Base
   # It returns the articles whose titles contain one or more words that form the query
   def self.search(query)
     # where(:title, query) -> This would return an exact match of the query
-    where("title like ?", "%#{query}%") 
+    where("title like ? or content like ?", "%#{query}%", "%#{query}%") 
   end
 #end
 
